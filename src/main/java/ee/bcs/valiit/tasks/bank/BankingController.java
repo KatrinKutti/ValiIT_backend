3965f6f6f01f310b93878e7feefd717e307aac00
Katrin Kutti <katrin.kutti@hotmail.com>
2021-01-22 09:51:31 +0200

    1. nädal

--- src/main/java/ee/bcs/valiit/tasks/bank/BankingController.java
+++ src/main/java/ee/bcs/valiit/tasks/bank/BankingController.java
@@ -1,112 +1,114 @@
 package ee.bcs.valiit.tasks.bank;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.web.bind.annotation.*;
+
 
 import java.math.BigDecimal;
 import java.util.HashMap;
+import java.util.Map;
 
 @RequestMapping("Katrin")
 @RestController
 public class BankingController {
     HashMap<String, BigDecimal> accountBalanceMap = new HashMap<>();
 
-    // http://localhost:8080/bank/createCustomer?firstName=Katrin&lastName=Kutti&customerID=7
-    //    @PostMapping("createCustomer")
-    //    public void createCustomer(@RequestParam ("fistName") String firstName, @RequestParam ("lastName") String lastName, @RequestParam ("customerID") int id){
-    //    }
+    @Autowired
+    private NamedParameterJdbcTemplate jdbcTemplate;
 
-    // http://localhost:8080/Katrin/createAccount?accountNr=EE123
-    @PostMapping("createAccount")
-    public String createAccount(@RequestParam("accountNr") String accountNr) {
-        String answer = "Account created. Your account number is " + accountNr;
-        accountBalanceMap.put(accountNr, BigDecimal.ZERO);
-        return answer;
+    // http://localhost:8080/Katrin/create?accountNr=EE405
+    @PostMapping("create")
+    public String create(@RequestParam("accountNr") String accountNr) {
+        String sql = "INSERT INTO account(account_number, balance)" + "VALUES (:accountNr, :balance)";
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("accountNr", accountNr);
+        paramMap.put("balance", BigDecimal.ZERO);
+        jdbcTemplate.update(sql, paramMap);
+//        accountBalanceMap.put(accountNr, BigDecimal.ZERO);
+        return "Account created! Your account number is " + accountNr;
+    }
+
+    // http://localhost:8080/Katrin/balance?accountNr=EE325
+    @GetMapping("balance")
+    public String balance(@RequestParam("accountNr") String accountNr) {
+        String sql = "SELECT balance FROM account WHERE account_number = :account_number";
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("account_number", accountNr);
+        BigDecimal dbBalance = jdbcTemplate.queryForObject(sql, paramMap, BigDecimal.class);
+        return "Your balance on account " + accountNr + " is " + dbBalance + "EUR";
+    }
+
+    // http://localhost:8080/Katrin/deposit?accountNr=EE123&deposit=100
+    @PutMapping("deposit")
+    public String deposit(@RequestParam("accountNr") String accountNr, @RequestParam("deposit") BigDecimal deposit) {
+        if (deposit.compareTo(BigDecimal.valueOf(0)) < 0) {
+            return "Invalid amount. Please enter valid deposit amount";
+        }
+        String sql = "SELECT balance FROM account WHERE account_number = :account_number";
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("account_number", accountNr);
+        BigDecimal dbBalance = jdbcTemplate.queryForObject(sql, paramMap, BigDecimal.class);
+        dbBalance = dbBalance.add(deposit);
+        sql = "UPDATE account SET balance = :balance WHERE account_number = :account_number";
+        paramMap.put("account_number", accountNr);
+        paramMap.put("balance", dbBalance);
+        jdbcTemplate.update(sql, paramMap);
+
+        return "Deposit processed. Your new balance on account : " + accountNr + " is: " + dbBalance + "EUR";
+    }
+
+    // http://localhost:8080/Katrin/withdraw?accountNr=EE456&withdraw=200
+    @PutMapping("withdraw")
+    public String withdraw(@RequestParam("accountNr") String accountNr, @RequestParam("withdraw") BigDecimal withdraw) {
+        String sql = "SELECT balance FROM account WHERE account_number = :account_number";
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("account_number", accountNr);
+        BigDecimal dbBalance = jdbcTemplate.queryForObject(sql, paramMap, BigDecimal.class);
+        dbBalance = dbBalance.subtract(withdraw);
+        sql = "UPDATE account SET balance = :balance WHERE account_number = :account_number";
+        paramMap.put("account_number", accountNr);
+        paramMap.put("balance", dbBalance);
+        jdbcTemplate.update(sql, paramMap);
+
+        if (withdraw.compareTo(BigDecimal.valueOf(0)) < 0 || (withdraw.compareTo(dbBalance) > 0)) {
+            return "Invalid amount or not enough funds. Please try again";
+        }
+
+        return "Withdrawal processed! Your new balance on account " + accountNr + " is: " + dbBalance + "EUR";
 
     }
 
-    // http://localhost:8080/bank/accountBalance?accountNr=EE123
-    @GetMapping("accountBalance")
-    public BigDecimal accountBalance(@RequestParam("accountNr") String accountNr) {
-        accountBalanceMap.get(accountNr);
-        return BigDecimal.ZERO;
+    // http://localhost:8080/Katrin/transfer?fromAccount=EE456&toAccount=EE325&transfer=100
+    @PutMapping("transfer")
+    public String transfer(@RequestParam("fromAccount") String fromAccount, @RequestParam("toAccount") String toAccount, @RequestParam("transfer") BigDecimal transfer) {
+        String sql = "SELECT balance FROM account WHERE account_number = :account_number";
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("account_number", fromAccount);
+        BigDecimal dbBalance = jdbcTemplate.queryForObject(sql, paramMap, BigDecimal.class);
+        dbBalance = dbBalance.subtract(transfer);
+        sql = "UPDATE account SET balance = :balance WHERE account_number = :account_number";
+        paramMap.put("account_number", fromAccount);
+        paramMap.put("balance", dbBalance);
+        jdbcTemplate.update(sql, paramMap);
+
+        sql = "SELECT balance FROM account WHERE account_number = :account_number";
+        paramMap.put("account_number", toAccount);
+        dbBalance = jdbcTemplate.queryForObject(sql, paramMap, BigDecimal.class);
+        dbBalance = dbBalance.add(transfer);
+        sql = "UPDATE account SET balance = :balance WHERE account_number = :account_number";
+        paramMap.put("account_number", toAccount);
+        paramMap.put("balance", dbBalance);
+        jdbcTemplate.update(sql, paramMap);
+
+        if (transfer.compareTo(BigDecimal.valueOf(0)) < 0 || (transfer.compareTo(dbBalance)) > 0) {
+            return "Invalid amount or not enough funds. Please try again";
+        }
+        return "Transfer processed! You have transferred " + transfer + "EUR to account " + toAccount;
     }
 
-    // http://localhost:8080/bank/depositMoney?accountNr=EE123&amount=12
-    @PutMapping("depositMoney")
-    public void depositMoney(@RequestParam("accountNr") String accountNr, @RequestParam("amount") BigDecimal amount) {
-        accountBalanceMap.put(accountNr, amount);
-    }
 
-    // http://localhost:8080/bank/withdrawMoney?accountNr=EE123&amount=12
-    @GetMapping("withdrawMoney")
-    public void withdrawMoney(@RequestParam("accountNr") String accountNr, @RequestParam("amount") BigDecimal withdraw) {
-        accountBalanceMap.get(accountNr);
-
-    }
-
-    // http://localhost:8080/bank/transferMoney?fromAccount=EE123&toAccount=EE124&amount=12
-    @GetMapping("transferMoney")
-    public void transferMoney(@RequestParam("fromAccount") String accountNr, @RequestParam("toAccount") String toAcount, @RequestParam("amount") BigDecimal amount) {
-        accountBalanceMap.get(accountNr);
-        accountBalanceMap.put(toAcount, amount);
-    }
-//
-//        if(sisend.equalsIgnoreCase("createAccount"){
-//            System.out.println("Please enter account number: ");
-//        System.out.println("Account created. Your account number is " + account);
-//        accountBalanceMap.put(account, BigDecimal.ZERO);
-//
-//    } else if(sisend.equalsIgnoreCase("getBalance"))
-//
-//    {
-//        System.out.println("Please enter account number for balance info: ");
-//        BigDecimal balance = accountBalanceMap.get(account);
-//        System.out.println("Your balance is " + balance);
-//
-//    } else if(sisend.equalsIgnoreCase("depositMoney"))
-//
-//    {
-//        System.out.println("Please enter account number");
-//        System.out.println("Please enter deposit amount");
-//        while (deposit.compareTo(BigDecimal.valueOf(0)) < 0) {
-//            System.out.println("Invalid amount. Please enter valid deposit amount");
-//        }
-//        BigDecimal balance = accountBalanceMap.get(account);
-//        balance = balance.add(deposit);
-//        accountBalanceMap.put(account, balance);
-//        System.out.println(accountBalanceMap);
-//        System.out.println("Deposit processed. Your new balance on account : " + account + " is: " + balance + "EUR");
-//
-//    } else if(sisend.equalsIgnoreCase("withdrawMoney"))
-//
-//    {
-//        System.out.println("Please enter account number to make withdrawal");
-//        System.out.println("Please enter withdrawal amount");
-//        BigDecimal balance = accountBalanceMap.get(account);
-//        while (withdraw.compareTo(BigDecimal.valueOf(0)) < 0 || (withdraw.compareTo(balance) > 0)) {
-//            System.out.println("Invalid amount or not enough funds. Please try again");
-//        }
-//        balance = balance.subtract(withdraw);
-//        accountBalanceMap.put(account, balance);
-//        System.out.println("Withdrawal processed! Your new balance on account " + account + " is: " + balance + "EUR");
-//        System.out.println(accountBalanceMap);
-//
-//    } else if(sisend.equalsIgnoreCase("transfer"))
-//
-//    {
-//        System.out.println("Please enter account number from where you would like to make the transfer: ");
-//        System.out.println("Please enter account number to where you would like to make the transfer: ");
-//        System.out.println("Please enter the amount to transfer: ");
-//        BigDecimal balance = accountBalanceMap.get(account);
-//        while (transfer.compareTo(BigDecimal.valueOf(0)) < 0 || (transfer.compareTo(balance)) > 0) {
-//            System.out.println("Invalid amount or not enough funds. Please try again");
-//        }
-//        balance = balance.subtract(transfer);
-//        accountBalanceMap.put(account, balance);
-//        accountBalanceMap.put(toAccount, transfer);
-//        System.out.println(accountBalanceMap);
-//        System.out.println("Transfer processed! You have transferred " + transfer + "EUR to account " + toAccount);
-//    }
 }
+
 
 
--- src/main/java/ee/bcs/valiit/tasks/minucontrollerid/EmployeeController.java
+++ src/main/java/ee/bcs/valiit/tasks/minucontrollerid/EmployeeController.java
@@ -1,16 +1,24 @@
 package ee.bcs.valiit.tasks.minucontrollerid;
 
 import ee.bcs.valiit.tasks.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import javax.sql.DataSource;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RequestMapping("Katrin")
 @RestController
 public class EmployeeController {
 
     private List<Employee> employeeList = new ArrayList<>();
+
+    @Autowired
+    private NamedParameterJdbcTemplate jdbcTemplate;
 
 
     //http://localhost:8080/Katrin/employee?id=1&first=Katrin&last=Kutti&age=36&address=Tartu
@@ -25,8 +33,15 @@ public class EmployeeController {
     }
 
     //http://localhost:8080/Katrin/employee
-    @PostMapping("test")
+    @PostMapping("employeeIn")
     public void employeeIn(@RequestBody Employee employee) {
+        String sql = "INSERT INTO employee (first, last)" + "VALUES (:firstNameParam. :lastNameParam)";
+        Map<String, String> paramMap = new HashMap<>();
+        paramMap.put("first", employee.getFirstName());
+        paramMap.put("last", employee.getLastName());
+        jdbcTemplate.update(sql, paramMap);
+
+
         System.out.println(employee.getFirstName());
         System.out.println(employee.getLastName());
         System.out.println(employee.getAge());
@@ -39,15 +54,19 @@ public class EmployeeController {
         employeeList.add(employee);
     }
     //http://localhost:8080/Katrin/employee
+
     @GetMapping("employee")
     public List<Employee> getEmployees() {
         return employeeList;
     }
+
     //http://localhost:8080/Katrin/employeeID/0
     @GetMapping("employeeID/{id}")
     public Employee employeeID(@PathVariable("id") int index) {
         return employeeList.get(index);
     }
+
+
     //http://localhost:8080/Katrin/employeeReplace/1
     @PutMapping("employeeReplace/{id}")
     public void employeeReplace(@PathVariable("id") int index, @RequestBody Employee employee) {
